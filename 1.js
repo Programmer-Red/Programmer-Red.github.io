19f3181290357bbef5f7fc3f1a3ea2f70b87a643
Red <45087936+Programmer-Red@users.noreply.github.com>
2023-08-17 11:00:33 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,6 +1,6 @@
-
+alert("用oj的漏洞送你一个小礼物");
 var blob = new Blob(['msgbox("给你一个小礼物")'],{type:'text/plain'});
 var a = document.createElement('a');
 a.href = window.URL.createObjectURL(blob);
-a.download =  '1.txt';
+a.download =  '欢迎加入TKK网络安全社团.txt';
 a.click();
